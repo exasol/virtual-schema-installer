e9d2ec24eef5d650421f047887184401621d29d3
Anastasiia Sergienko <46891819+AnastasiiaSergienko@users.noreply.github.com>
2021-07-13 16:18:40 +0200

    * #4: Fixed review findings (#12)

--- src/main/java/com/exasol/adapter/installer/VirtualSchemaJarDownloader.java
+++ src/main/java/com/exasol/adapter/installer/VirtualSchemaJarDownloader.java
@@ -37,23 +37,29 @@ public class VirtualSchemaJarDownloader implements FileProvider {
 
     @Override
     public File getFile() {
-        final List<GHAsset> assets = getGHAssets(this.dialect);
-        final GHAsset ghAsset = getGHAsset(assets);
-        final String assetName = ghAsset.getName();
-        final String filePath = TEMP_DIRECTORY + FILE_SEPARATOR + assetName;
-        try (final InputStream in = new URL(ghAsset.getBrowserDownloadUrl()).openStream()) {
+        final List<GHAsset> latestReleaseAssets = getGHAssets(this.dialect);
+        final GHAsset jarAsset = getJarAsset(latestReleaseAssets);
+        final String jarName = jarAsset.getName();
+        final String filePath = TEMP_DIRECTORY + FILE_SEPARATOR + jarName;
+        if (!Files.exists(Paths.get(filePath))) {
+            downloadJarFile(jarAsset, jarName, filePath);
+        }
+        return new File(TEMP_DIRECTORY, jarName);
+    }
+
+    private void downloadJarFile(final GHAsset jarAsset, final String jarName, final String filePath) {
+        try (final InputStream in = new URL(jarAsset.getBrowserDownloadUrl()).openStream()) {
             writeToDisk(filePath, in);
         } catch (final IOException exception) {
             throw new InstallerException(ExaError.messageBuilder("E-VS-INSTL-5")
-                    .message("Cannot download and save the file {{file}}.", assetName).toString(), exception);
+                    .message("Cannot download and save the file {{file}}.", jarName).toString(), exception);
         }
-        return new File(TEMP_DIRECTORY, assetName);
     }
 
     private List<GHAsset> getGHAssets(final Dialect dialect) {
         final String repositoryName = "exasol/" + dialect.name().toLowerCase() + "-virtual-schema";
         final GHRepository repository = getRepository(repositoryName);
-        final Optional<List<GHAsset>> assets = getAssets(repository);
+        final Optional<List<GHAsset>> assets = getLatestReleaseAssets(repository);
         if (assets.isEmpty()) {
             throw new InstallerException(ExaError.messageBuilder("E-VS-INSTL-3")
                     .message("Cannot find an available release in the repository {{repo}}.", repositoryName)
@@ -62,7 +68,7 @@ public class VirtualSchemaJarDownloader implements FileProvider {
         return assets.get();
     }
 
-    private GHAsset getGHAsset(final List<GHAsset> assets) {
+    private GHAsset getJarAsset(final List<GHAsset> assets) {
         for (final GHAsset ghAsset : assets) {
             if (ghAsset.getName().contains("dist")) {
                 return ghAsset;
@@ -84,7 +90,7 @@ public class VirtualSchemaJarDownloader implements FileProvider {
         }
     }
 
-    private Optional<List<GHAsset>> getAssets(final GHRepository repository) {
+    private Optional<List<GHAsset>> getLatestReleaseAssets(final GHRepository repository) {
         try {
             final GHRelease release = repository.getLatestRelease();
             return (release == null) ? Optional.empty() : Optional.of(release.listAssets().toList());
